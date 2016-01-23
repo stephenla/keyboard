c09ad4556417ff5995ecca6796187c2004c79b21
Stephen La <lastephen0@gmail.com>
2016-01-23 10:33:31 -0800

    keyboard, keys, events

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       dist: {
-        src: ['lib/js/views/*.js','lib/js/controllers/*.js','lib/js/models/*.js', 'lib/js/apps/*.js','lib/js/routers/*.js'],
+        src: ['lib/js/views/*.js', 'lib/js/controllers/*.js', 'lib/js/models/*.js', 'lib/js/apps/*.js', 'lib/js/routers/*.js'],
         dest: 'lib/js/main.js'
       },
       test: {
@@ -13,18 +13,31 @@ module.exports = function(grunt) {
         dest: 'spec/spec/spec.js'
       }
     },
+    sass: { 
+      dist: {
+        files: {
+          'lib/css/styles.css': 'lib/css/styles.scss'
+        }
+      }
+    },
     watch: {
       scripts: {
         files: ['lib/js/**/*.js', '!lib/js/main.js', 'spec/spec/**/*.js', '!spec/spec/spec.js'],
         tasks: ['concat']
+      },
+      styles: {
+        files: 'lib/css/styles.scss',
+        tasks: ['sass']
       }
     }
+    
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   // Default task(s).
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat', 'sass']);
 
 };
